5a494cf4383d62bb19147449cde84e49f5f42ce5
munchmtxi <munchmalawi@gmail.com>
2025-02-12 12:42:12 +0000

    migration issues

--- migrations/20250127001158-create-notification.js
+++ migrations/20250127001158-create-notification.js
@@ -48,6 +48,13 @@ module.exports = {
           isInt: { msg: 'Booking ID must be an integer' },
         }
       },
+      template_id: { // New field added for template_id
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        validate: {
+          isInt: { msg: 'Template ID must be an integer' },
+        }
+      },
       type: {
         type: Sequelize.STRING,
         allowNull: false,
@@ -70,6 +77,13 @@ module.exports = {
       read_status: {
         type: Sequelize.BOOLEAN,
         defaultValue: false,
+      },
+      action_url: {
+        type: Sequelize.STRING,
+        allowNull: true, // New field for action URLs
+        validate: {
+          isUrl: { msg: 'Action URL must be a valid URL' },
+        }
       },
       created_at: {
         allowNull: false,
@@ -100,12 +114,25 @@ module.exports = {
     await queryInterface.addIndex('notifications', ['booking_id'], {
       name: 'notifications_booking_id_index'
     });
+
+    // Adding index on action_url for performance
+    await queryInterface.addIndex('notifications', ['action_url'], {
+      name: 'notifications_action_url_index'
+    });
+
+    // Adding index on template_id
+    await queryInterface.addIndex('notifications', ['template_id'], {
+      name: 'notifications_template_id_index'
+    });
   },
+
   async down(queryInterface, Sequelize) {
     // Remove indexes first
     await queryInterface.removeIndex('notifications', 'notifications_user_id_index');
     await queryInterface.removeIndex('notifications', 'notifications_order_id_index');
     await queryInterface.removeIndex('notifications', 'notifications_booking_id_index');
+    await queryInterface.removeIndex('notifications', 'notifications_action_url_index');
+    await queryInterface.removeIndex('notifications', 'notifications_template_id_index');
 
     // Drop the table
     await queryInterface.dropTable('notifications');
